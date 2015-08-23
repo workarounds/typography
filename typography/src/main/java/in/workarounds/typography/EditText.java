6de11bc7a5a4e3ab5049738bece409e9e8feaa00
themadhukiran <pmadhukiran96@gmail.com>
2015-08-23 23:03:08 +0530

    add EditText functionality

---
+++ typography/src/main/java/in/workarounds/typography/EditText.java
@@ -0,0 +1,51 @@
+package in.workarounds.typography;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+/**
+ * Created by madki on 23/08/15.
+ */
+public class EditText extends android.widget.EditText {
+    private String mFontName;
+    private String mFontVariant;
+
+    public EditText(Context context) {
+        super(context);
+        setFont();
+    }
+
+    public EditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getAttrs(attrs);
+        setFont();
+    }
+
+    public EditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        getAttrs(attrs);
+        setFont();
+    }
+
+    private void getAttrs(AttributeSet attrs){
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.EditText);
+        try{
+            mFontName = a.getString(R.styleable.EditText_font_name);
+            mFontVariant = a.getString(R.styleable.EditText_font_variant);
+        } finally{
+            a.recycle();
+        }
+    }
+
+    private void setFont() {
+        if(isInEditMode()) {
+            return;
+        }
+
+        Typeface typeface = FontLoader.getInstance(getContext()).getTypeface(mFontName, mFontVariant);
+        setTypeface(typeface);
+    }
+
+}
