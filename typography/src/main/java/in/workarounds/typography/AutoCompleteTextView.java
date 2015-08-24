48d423c383b679b68fa881028e184ee6386f72cc
themadhukiran <pmadhukiran96@gmail.com>
2015-08-24 17:34:15 +0530

    add support for AutoCompleteTextView

---
+++ typography/src/main/java/in/workarounds/typography/AutoCompleteTextView.java
@@ -0,0 +1,51 @@
+package in.workarounds.typography;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+/**
+ * Created by madki on 24/08/15.
+ */
+public class AutoCompleteTextView extends android.widget.AutoCompleteTextView {
+    private String mFontName;
+    private String mFontVariant;
+
+    public AutoCompleteTextView(Context context) {
+        super(context);
+        setFont();
+    }
+
+    public AutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getAttrs(attrs);
+        setFont();
+    }
+
+    public AutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        getAttrs(attrs);
+        setFont();
+    }
+
+    private void getAttrs(AttributeSet attrs){
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.EditText);
+        try{
+            mFontName = a.getString(R.styleable.AutoCompleteTextView_font_name);
+            mFontVariant = a.getString(R.styleable.AutoCompleteTextView_font_variant);
+        } finally{
+            a.recycle();
+        }
+    }
+
+    private void setFont() {
+        if(isInEditMode()) {
+            return;
+        }
+
+        Typeface typeface = FontLoader.getInstance(getContext()).getTypeface(mFontName, mFontVariant);
+        setTypeface(typeface);
+    }
+
+}
