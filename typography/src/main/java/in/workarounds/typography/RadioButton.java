91b6240825cf6ec5dd3d0f60eb443d1151b914d2
themadhukiran <pmadhukiran96@gmail.com>
2015-10-13 14:38:15 +0530

    add support to checkbox, radiobutton, togglebutton

---
+++ typography/src/main/java/in/workarounds/typography/RadioButton.java
@@ -0,0 +1,50 @@
+package in.workarounds.typography;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+/**
+ * Created by madki on 13/10/15.
+ */
+public class RadioButton extends android.widget.RadioButton {
+    private String mFontName;
+    private String mFontVariant;
+
+    public RadioButton(Context context) {
+        super(context);
+        setFont();
+    }
+
+    public RadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getAttrs(attrs);
+        setFont();
+    }
+
+    public RadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        getAttrs(attrs);
+        setFont();
+    }
+
+    private void getAttrs(AttributeSet attrs){
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TextView);
+        try{
+            mFontName = a.getString(R.styleable.TextView_font_name);
+            mFontVariant = a.getString(R.styleable.TextView_font_variant);
+        } finally{
+            a.recycle();
+        }
+    }
+
+    private void setFont() {
+        if(isInEditMode()) {
+            return;
+        }
+
+        Typeface typeface = FontLoader.getInstance(getContext()).getTypeface(mFontName, mFontVariant);
+        setTypeface(typeface);
+    }
+}
